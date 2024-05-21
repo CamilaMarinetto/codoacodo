abf76a2e02baad0a83561849cd38360fd42c2120
camilamarinetto <camilamarinetto21@gmail.com>
2024-05-21 12:42:27 -0300

    Agregue Mi perfil

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var icono = document.getElementById("flecha");
   
     setTimeout(function() {
-      icono.style.opacity = "1";}, 1000); 
-  });
+      icono.style.opacity = "1";}, 1800); 
+  })
   
 
 //para q la flecha desaparezca gradualmente cuando se scrolee para abajo
@@ -13,16 +13,17 @@ window.addEventListener("scroll", function() {
   var currentScroll = document.documentElement.scrollTop;
   var flecha = document.getElementById("flecha");
   
-  if (currentScroll > lastScrollTop) {
-    // Scroll hacia abajo
+  if (currentScroll > lastScrollTop && currentScroll > 0) {
+    // Scroll hacia abajo y no estamos en la parte superior de la página
     flecha.style.opacity = "0"; // Establece la opacidad de la flecha a 0
   } else {
-    // Scroll hacia arriba
-    flecha.style.opacity = "1"; // Establece la opacidad de la flecha a 1
+    // Estamos en la parte superior de la página o estamos volviendo hacia arriba
+    flecha.style.opacity = currentScroll === 0 ? "1" : "0"; // Establece la opacidad de la flecha a 1 solo si estamos en la parte superior de la página
   }
   
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 }, false);
+
 
 
 
